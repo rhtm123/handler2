c21c01501d9e30977359a2b9d85131fbde77d934
rhtm123 <maurya.iitk@gmail.com>
2023-03-03 15:55:37 +0530

    container urls added

--- container/urls.py
+++ container/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from container import views
 
 urlpatterns = [
-    path('create-new-container/', views.create_new_container),
-    path('save-code/',views.save_code),
+    path('create-new-container', views.create_new_container),
+    path('save-code',views.save_code),
 ]
