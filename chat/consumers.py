f1adc59086739f4b4b4dfab924c8eb1cb463337a
Rohit Maurya <maurya.iitk@gmail.com>
2023-10-11 11:21:25 +0530

    Update consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -86,7 +86,7 @@ def delete_nginx_config(container_name):
 
 def reload_nginx():
     # Test Nginx configuration and reload if it's valid
-    run_process("sudo nginx -t", "temp/outcome1.txt")
+    run_process("sudo nginx -t", "tmp/outcome1.txt")
     run_process("sudo nginx -s reload", "tmp/outcome2.txt")
     print("nginx reload successful")
 
